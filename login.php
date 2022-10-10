055f47ca4b51946e1703ac73d61716ef09c4bf5c
AOrps <aeo34@njit.edu>
2022-10-09 23:15:30 -0400

    p config

---
+++ login.php
@@ -0,0 +1,13 @@
+<?php
+    phpinfo();
+?>
+
+<html>
+<body>
+    <form method="POST">
+        <input type="text" id="username" placeholder="Username / Email">
+        <input type="password" id="password" placeholder="Password">
+        <input type="submit">
+    </form>
+</body>
+</html>
